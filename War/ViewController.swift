aa03f3d04cce7da92ccc9b388ab2cdbf24f1d656
Mohamed Mostafa Fawzi <47675332+MohamedMostafaFawzi@users.noreply.github.com>
2020-01-25 04:26:13 +0200

    adding the xcode project files
    I added the xcode project files for the first time.

---
+++ War/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Mohamed Canaria on 8/10/19.
+//  Copyright © 2019 MohamedCanaria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var leftviewimage: UIImageView!
+    @IBOutlet weak var rightviewimage: UIImageView!
+    @IBOutlet weak var playerscore: UILabel!
+    @IBOutlet weak var cpuscore: UILabel!
+    
+    var leftscore = 0
+    var rightscore = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func dealtapped(_ sender: Any) {
+        
+        // Randomize numbers
+        let leftnumber = Int.random(in: 2...4)
+        let rightnumber = Int.random(in: 2...4)
+        
+        // Update the image view
+        leftviewimage.image = UIImage(named: "card\(leftnumber)")
+        rightviewimage.image = UIImage(named: "card\(rightnumber)")
+        
+        // Compare the random numbers
+        if leftnumber > rightnumber {
+            
+           // left wins
+             leftscore += 1
+            playerscore.text = String(leftscore)
+            
+        }
+        
+        else if leftnumber < rightnumber {
+            
+            // right wins
+            rightscore += 1
+            cpuscore.text = String(rightscore)
+            
+        }
+        
+        else {
+            
+            // draw
+        }
+        
+        
+    }
+    
+    
+    
+    
+}
+
